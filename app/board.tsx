86ed17051bf92a052c0c6a3832791bfbdfb0f6bd
Savo Vuksan <savovuksan@gmail.com>
2024-07-09 19:24:21 +0200

    Added option to delete and create cards

--- app/board.tsx
+++ app/board.tsx
@@ -1,14 +1,77 @@
-import Card from "./card";
+import React, { ChangeEvent, useState } from "react";
+import Card, { CardProps } from "./card";
 
 type BoardProps = {
   title: string;
 };
 
 export default function Board({ title }: BoardProps) {
+  const [cards, setCards] = useState<Array<CardProps>>([
+    { id: 0, text: "test" },
+    { id: 1, text: "test2" },
+    { id: 2, text: "", isDeletable: false },
+  ]);
+
+  function handleCardChange(
+    event: React.ChangeEvent<HTMLInputElement>,
+    card: CardProps
+  ) {
+    const index = cards.findIndex((element) => element.id === card.id);
+    const newCards = cards.slice();
+    newCards[index] = { ...card, text: event.target.value };
+    setCards(newCards);
+  }
+
+  function handleCardDelete(
+    event: React.MouseEvent<HTMLButtonElement>,
+    card: CardProps
+  ) {
+    setCards(cards.slice().filter((element) => element.id !== card.id));
+  }
+
+  function handleCardCreate(
+    event: React.KeyboardEvent<HTMLInputElement>,
+    card: CardProps
+  ) {
+    const resetCard = cards.find((element) => element.id === card.id);
+    const resetCardIndex = cards.findIndex((element) => element.id === card.id);
+    const newCards = cards.slice();
+    const newCard = createEmptyCard();
+    resetCard!.isDeletable = true;
+    newCards[resetCardIndex] = resetCard!;
+    newCards.push(newCard);
+    setCards(newCards);
+  }
+
+  function createEmptyCard(): CardProps {
+    return {
+      id: constantCounter(),
+      text: "",
+      isDeletable: false,
+    };
+  }
+
   return (
     <div className="rounded p-4 shadow m-4 max-w-sm flex gap-4 flex-col">
       <h1 className="text-xl text-blue-500">{title}</h1>
-      <Card></Card>
+      {cards.map((element, index) => {
+        return (
+          <Card
+            id={element.id}
+            key={element.id}
+            text={element.text}
+            onChange={(event) => handleCardChange(event, element)}
+            onDelete={(event) => handleCardDelete(event, element)}
+            onEnter={(event) => handleCardCreate(event, element)}
+            isDeletable={element.isDeletable}
+          ></Card>
+        );
+      })}
     </div>
   );
+}
+let counter = 3;
+function constantCounter() {
+  counter++;
+  return counter - 1;
 }
--- app/card.tsx
+++ app/card.tsx
@@ -1,10 +1,40 @@
 import FeatherIcon from "feather-icons-react";
+import React, { ChangeEventHandler } from "react";
 
-export default function Card() {
+export type CardProps = {
+  id: number;
+  text: string;
+  isDeletable?: boolean;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  onDelete?: React.MouseEventHandler<HTMLButtonElement>;
+  onEnter?: React.KeyboardEventHandler<HTMLInputElement>;
+};
+
+export default function Card({
+  text,
+  isDeletable,
+  onChange,
+  onDelete,
+  onEnter,
+}: CardProps) {
   return (
     <div className="flex flex-row gap-2 items-center">
-      <input placeholder="Card" type="text" className="flex-1 p-1" />
-      <FeatherIcon icon="x"></FeatherIcon>
+      <input
+        placeholder="Card"
+        type="text"
+        value={text}
+        className="flex-1 p-1"
+        onChange={onChange}
+        onKeyUp={(event) => {
+          event.key === "Enter" && onEnter ? onEnter(event) : undefined;
+        }}
+      />
+      <button
+        onClick={onDelete}
+        hidden={isDeletable === undefined ? false : !isDeletable}
+      >
+        <FeatherIcon icon="x"></FeatherIcon>
+      </button>
     </div>
   );
 }
